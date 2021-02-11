bebc407e2b20a19756b75a0522b1687582090dcd
takahiro <ttakka365@gmail.com>
2021-02-11 12:39:24 +0900

    Delete Directory

--- sample_app/app/Http/Controllers/Task_appController.php
+++ sample_app/app/Http/Controllers/Task_appController.php
@@ -11,49 +11,131 @@ class Task_appController extends Controller
         $title = 'To Doリスト';
         // Messageモデルを利用してmessageの一覧を取得
 
-        if($request->sort === "asc") {
-            $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortBy('date');
-        } else {
+        if($request->sort === "desc") {
             $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortByDesc('date');
+        } else {
+            $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortBy('date');
         }
+
+        if($request->status === "in") {
+            $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortByDesc('status');
+        } else if($request->status === "out") {
+            $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortBy('status');
+        }
+
+        if($request->category === "up") {
+            $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortByDesc('category');
+        }
+
+        if($request->sql_kind === "category_name") {
+            if($request->category_view === "all") {
+                $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->sortBy('date');
+            } else {
+                $task_apps = \App\task_app::all()->where('users_id',Auth::user()->id)->where('category', $request->category_view);
+            }
+        }
+        $category_name = $request->category_view;
+
+        $task_apps_archive = \App\taks_apps_archive::where('users_id',Auth::user()->id)->paginate(10);
+
+        $categories = \App\category::all()->where('users_id',Auth::user()->id);
 
         // views/messages/index.blade.phpを指定
         return view('task_app.index_task_app',[
             'title' => $title,
             'task_apps' => $task_apps,
+            'task_apps_archive' => $task_apps_archive,
+            'categories' => $categories,
+            'category_name' => $category_name
         ]);
+        
     }
 
     public function create(Request $request){
 
-        // requestオブジェクトのvalidateメソッドを利用。
-        $request->validate([
-            'body' => 'required|max:100', 
-            'date' => 'required'// nameは必須、20文字以内
+        if($request->body === null) {
+            
+            if($request->status !== null) {
+
+                $request->validate([
+                    'body' => 'required|max:20', 
+                ]);
+
+            }
+
+            $request->validate([
+                'category_name' => 'required|unique:categories,category_name|max:20'
+            ]);
+
+            $category = new \App\category;
+    
+            $category->id = $request->id;
+            $category->category_name = $request->category_name;
+            $category->users_id = Auth::user()->id;
+    
+            $category->save();
+    
+            return redirect('/task_apps')->with('status', 'カテゴリを登録しました！');
+
+        } else {
+
+            // requestオブジェクトのvalidateメソッドを利用。
+            $request->validate([
+                'body' => 'required|max:20', 
+                'description' => 'max:300'
+            ]);
+
+            // Messageモデルを利用して空のMessageオブジェクトを作成
+            $task_app = new \App\task_app;
+            $category = \App\category::find($request->category_id);
+
+            // フォームから送られた値を設定
+            $task_app->id = $request->id;
+            $task_app->body = $request->body;
+            $task_app->date = $request->date;
+            $task_app->status = $request->status;
+            if($request->date === null) {
+                $task_app->status = 3;
+            }
+            $task_app->description = $request->description;
+            $task_app->category_id = $request->category_id;
+            if($request->category_id !== "0"){
+                $task_app->category = $category->category_name;
+            }
+            $task_app->users_id = Auth::user()->id;
+
+            // messagesテーブルにINSERT
+            $task_app->save();
+
+            // メッセージ一覧ページにリダイレクト
+            return redirect('/task_apps')->with('status', 'リストに入れました！ファイト！');
+
+        }       
+    }
+
+    public function index_task_app_category(Request $request) {
+
+        $title = 'To Doリスト';
+        $categories = \App\category::all()->where('users_id',Auth::user()->id);
+
+        // views/messages/index.blade.phpを指定
+        return view('task_app.index_task_app_category',[
+            'title' => $title,
+            'categories' => $categories
         ]);
-
-        // Messageモデルを利用して空のMessageオブジェクトを作成
-        $task_app = new \App\task_app;
-
-        // フォームから送られた値を設定
-        $task_app->id = $request->id;
-        $task_app->body = $request->body;
-        $task_app->date = $request->date;
-        $task_app->status = $request->status;
-        $task_app->users_id = Auth::user()->id;
-
-        // messagesテーブルにINSERT
-        $task_app->save();
-
-        // メッセージ一覧ページにリダイレクト
-        return redirect('/task_apps')->with('status', 'リストに入れました！ファイト！');
     }
 
     public function update(Request $request){
 
         $task_app = \App\task_app::find($request->id);
+        $category = \App\category::find($request->id);
 
         if($request->sql_kind === "update_text") {
+
+            // requestオブジェクトのvalidateメソッドを利用。
+            $request->validate([
+                'body' => 'required|max:20'
+            ]);
 
             $task_app->body = $request->body;
             $task_app->save();
@@ -63,10 +145,55 @@ class Task_appController extends Controller
         } else if($request->sql_kind === "update_date"){
 
             $task_app->date = $request->date;
+            $task_app->status = 0;
             $task_app->save();
 
             return redirect('/task_apps')->with('status', '締め切り日変更完了！');
 
+        } else if($request->sql_kind === "update_text_description"){
+
+            $request->validate([
+                'description' => 'required|max:300'
+            ]);
+
+            $task_app->description = $request->description;
+            $task_app->save();
+
+            return redirect('/task_apps')->with('status', '詳細の変更完了！');
+
+        } else if($request->sql_kind === "update_category"){
+            $category = \App\category::find($request->category_id);
+            $task_app->category_id = $request->category_id;
+            if($request->category_id !== "0"){
+                $task_app->category = $category->category_name;
+            } else {
+                $task_app->category = "未設定";
+            }
+            $task_app->save();
+
+            return redirect('/task_apps')->with('status', 'カテゴリーの変更完了！');
+        
+        } else if($request->sql_kind === "change_category"){
+
+            $request->validate([
+                'category_name' => 'required|unique:categories,category_name|max:20'
+            ]);
+
+            $category->category_name = $request->category_name;
+
+            $task_apps_of_category = \App\task_app::get()->where('category_id',$request->id);
+
+            if(count($task_apps_of_category) !== 0){
+                $category_of_task_apps = \App\category::find($request->id)->task_app::get()->where('category_id',$request->id);
+                foreach($category_of_task_apps as $category_of_task_app) {
+                    $category_of_task_app->category = $request->category_name;
+                    $category_of_task_app->save();
+                }
+            }
+            $category->save();
+
+            return redirect('/task_apps_category')->with('status', 'カテゴリーの変更完了！');
+        
         } else {
             
             if($task_app->status === 0) {
@@ -83,12 +210,42 @@ class Task_appController extends Controller
 
     public function destroy(Request $request) {
 
+        $task_app = \App\task_app::find($request->body);
+        $task_app = \App\task_app::find($request->description);
+        $task_app = \App\task_app::find($request->category);
         $task_app = \App\task_app::find($request->id);
+        $category = \App\category::find($request->category_name);
+        $category = \App\category::find($request->id);
 
+        if($request->body !== NULL) {
+
+            $task_app_archive = new \App\taks_apps_archive;
+            // フォームから送られた値を設定
+            $task_app_archive->task_apps_id = $request->id;
+            $task_app_archive->body = $request->body;
+            $task_app_archive->description = $request->description;
+            $task_app_archive->category = $request->category;
+            $task_app_archive->users_id = Auth::user()->id;
+            $task_app_archive->save();
+
+            $task_app->delete();
+
+            // メッセージ一覧ページにリダイレクト
+            return redirect('/task_apps')->with('status', 'リストから削除しました！お疲れさまでした！');
+
+        }
+        //カテゴリーの削除
+        if($request->category_name !== NULL) {
+
+            $category->delete();
+
+            return redirect('/task_apps');
+        }
+
+        //取り消しボタン
         $task_app->delete();
 
-        // メッセージ一覧ページにリダイレクト
-        return redirect('/task_apps')->with('status', '削除しました！お疲れさまでした！');
+        return redirect('/task_apps')->with('status', '取り消しました！');;
     }
 
     public function __construct() {
---
+++ sample_app/public/js/main.js
@@ -0,0 +1,115 @@
+$(function(){
+    $('div.text').click(function(){
+            if(!$(this).hasClass('on')){
+                $(this).addClass('on');
+                var txt = $(this).text();
+                $(this).html('<input type="text" name="body" value="'+txt+'" />');
+                $('div.text > input').focus().blur(function(){
+                var inputVal = $(this).val();
+                //編集が終わったらtextで置き換える
+                $(this).parent().removeClass('on').text(inputVal);
+                });
+            };
+    });
+    $('div.text_description').click(function(){
+        if(!$(this).hasClass('on')){
+            $(this).addClass('on');
+            var txt = $(this).text();
+            $(this).html('<textarea name="description" class="remark_field form-control">'+txt+'</textarea><input type="submit" class="btn btn-primary change_description" value="変更">');
+            $('div.text_description > textarea').focus().blur(function(){});
+        };
+    });
+    $('.date').click(function(){
+        if(!$(this).hasClass('on')){
+            $(this).addClass('on');
+            var txt = $(this).text();
+            $(this).html('<input type="date" name="date" class="show_date" value="'+txt+'" /><input type="submit" class="change_date" value="変更">');
+        };
+    });
+    
+    $('div.text_category').click(function(){
+        if(!$(this).hasClass('on')){
+            $(this).addClass('on');
+            var txt = $(this).text();
+            $(this).html('<input type="text" name="category_name" value="'+txt+'" />');
+            $('div.text > input').focus().blur(function(){
+            var inputVal = $(this).val();
+            //編集が終わったらtextで置き換える
+            $(this).parent().removeClass('on').text(inputVal);
+            });
+        };
+    });
+
+    /*完了ボタン*/
+    $('.delete').click(function(){
+        if(confirm('本当に完了にしますか？')){
+            /* キャンセルの時の処理 */
+            location.href = './index_task_app.blade.php';
+        }else{
+            /*　OKの時の処理 */
+            return false;
+        }
+    });
+
+    /*取り消しボタン*/
+    $('.cancel').click(function(){
+        if(confirm('取り消しますか？')){
+            /* キャンセルの時の処理 */
+            location.href = './index_task_app.blade.php';
+        }else{
+            /*　OKの時の処理 */
+            return false;
+        }
+    });
+
+    $('span.category').click(function(){
+            $(this).css("display", "none");
+            $(this).parent().nextAll('.category_choice').css("display", "inline");
+    });
+
+    $(".text").change(function(){
+        $("#submit_form").submit();
+    });
+
+    //ポップアップ関連
+    $('.description .btn').on('click',function(){
+        $(this).parent().nextAll('.popup').addClass('show');
+        $(".table-responsive").css("overflow-x", "visible");
+        $(".table-responsive").css("positon", "relative");
+    });
+    $('.back').on('click',function(){
+        $('.popup').removeClass('show');
+        $(".table-responsive").css("overflow-x", "auto");
+        $(".table-responsive").css("positon", "static");
+    });
+
+    //テーブルの色
+    $("tr td:has(.moge)").parent().addClass("parent");
+    
+    $("tr td:has(.false)").parent().addClass("underway");
+
+    $(".status_popup").on("click", function(){
+        $(this).hide("status_popup");
+    });
+    if($(window).width() < 768) {
+        $('.empty_msg').wrap('<div />').parent().addClass("empty");
+    };
+
+    // ①タブをクリックしたら発動
+    $('.tab li').click(function() {
+
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab li').index(this);
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active');
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active');
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.list li').removeClass('show_tab').eq(index).addClass('show_tab');
+
+    });
+
+    $("textarea").attr("rows", 1).on("input", e => {
+        $(e.target).height(0).innerHeight(e.target.scrollHeight);
+    });
+});
